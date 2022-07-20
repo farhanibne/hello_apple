38f0ed1a5fc465a0b20cdcf74290caad16e1a9aa
farhan ibne saif <84982846+farhanibne@users.noreply.github.com>
2022-07-20 11:00:56 +0600

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  try
+//
+//  Created by Farhan on 20/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, dz. welcome on ios")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tryApp.swift
@@ -0,0 +1,17 @@
+//
+//  tryApp.swift
+//  try
+//
+//  Created by Farhan on 20/7/22.
+//
+
+import SwiftUI
+
+@main
+struct tryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
